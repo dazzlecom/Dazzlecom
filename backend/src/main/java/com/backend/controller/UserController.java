8d831b766f45be6386f8ffdc62402e1f1e030826
krHasan <hasanbappi@gmail.com>
2018-10-25 01:59:40 +0600

    project initialization

---
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/")
+	public String test() {
+		return "this is hello from backend";
+	}
+	
+}
