be417a2cdbb6953c9985fd7b6877cf2bc9cc4ee5
darahat <darahat42@gmail.com>
2018-10-26 12:17:09 +0600

    dist update

--- backend/src/main/java/com/backend/controller/BackendController.java
+++ backend/src/main/java/com/backend/controller/BackendController.java
@@ -20,34 +20,34 @@ import com.backend.repository.UserRepository;
 @RequestMapping("/api")
 public class BackendController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(BackendController.class);
+    // private static final Logger LOG = LoggerFactory.getLogger(BackendController.class);
 
-    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+    // public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
 
-    @Autowired
-    private UserRepository userRepository;
+    // @Autowired
+    // private UserRepository userRepository;
 
-    @RequestMapping(path = "/hello")
-    public @ResponseBody String sayHello() {
-        LOG.info("GET called on /hello resource");
-        return HELLO_TEXT;
-    }
+    // @RequestMapping(path = "/hello")
+    // public @ResponseBody String sayHello() {
+    //     LOG.info("GET called on /hello resource");
+    //     return HELLO_TEXT;
+    // }
 
-    @RequestMapping(path = "/user", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    public @ResponseBody long addNewUser (@RequestParam String firstName, @RequestParam String lastName) {
-        User user = new User(firstName, lastName);
-        userRepository.save(user);
+    // @RequestMapping(path = "/user", method = RequestMethod.POST)
+    // @ResponseStatus(HttpStatus.CREATED)
+    // public @ResponseBody long addNewUser (@RequestParam String firstName, @RequestParam String lastName) {
+    //     User user = new User(firstName, lastName);
+    //     userRepository.save(user);
 
-        LOG.info(user.toString() + " successfully saved into DB");
+    //     LOG.info(user.toString() + " successfully saved into DB");
 
-        return user.getId();
-    }
+    //     return user.getId();
+    // }
 
-    @GetMapping(path="/user/{id}")
-    public @ResponseBody User getUserById(@PathVariable("id") long id) {
-        LOG.info("Reading user with id " + id + " from database.");
-        return userRepository.findById(id).get();
-    }
+    // @GetMapping(path="/user/{id}")
+    // public @ResponseBody User getUserById(@PathVariable("id") long id) {
+    //     LOG.info("Reading user with id " + id + " from database.");
+    //     return userRepository.findById(id).get();
+    // }
 
 }
